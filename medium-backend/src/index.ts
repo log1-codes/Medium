c4777000a30948a442b6c747bfdbf156430837bd
log1-codes <singhanurag1309@gmail.com>
2025-08-15 00:09:52 +0530

    init

---
+++ medium-backend/src/index.ts
@@ -0,0 +1,29 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+export default app
